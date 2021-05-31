dd27e0443f55afe946622841f1196f1a6b0e2096
Vladimír Urík <gggedrvideos@gmail.com>
2021-05-31 18:29:00 +0200

    Add autostart

--- app/Http/Controllers/Api/Remote/Servers/ServerInstallController.php
+++ app/Http/Controllers/Api/Remote/Servers/ServerInstallController.php
@@ -4,16 +4,27 @@ namespace Pterodactyl\Http\Controllers\Api\Remote\Servers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Pterodactyl\Models\Node;
 use Pterodactyl\Models\Server;
+use GuzzleHttp\Client;
 use Illuminate\Http\JsonResponse;
 use Pterodactyl\Http\Controllers\Controller;
 use Pterodactyl\Repositories\Eloquent\ServerRepository;
 use Pterodactyl\Http\Requests\Api\Remote\InstallationDataRequest;
 use Pterodactyl\Events\Server\Installed as ServerInstalled;
 use Illuminate\Contracts\Events\Dispatcher as EventDispatcher;
+use Pterodactyl\Repositories\Wings\DaemonPowerRepository;
+use Webmozart\Assert\Assert;
+use Illuminate\Contracts\Foundation\Application;
 
 class ServerInstallController extends Controller
 {
+
+    /**
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
     /**
      * @var \Pterodactyl\Repositories\Eloquent\ServerRepository
      */
@@ -27,10 +38,11 @@ class ServerInstallController extends Controller
     /**
      * ServerInstallController constructor.
      */
-    public function __construct(ServerRepository $repository, EventDispatcher $eventDispatcher)
+    public function __construct(ServerRepository $repository, EventDispatcher $eventDispatcher, Application $application)
     {
         $this->repository = $repository;
         $this->eventDispatcher = $eventDispatcher;
+        $this->app = $application;
     }
 
     /**
@@ -73,9 +85,31 @@ class ServerInstallController extends Controller
 
         // If the server successfully installed, fire installed event.
         if ($status === null) {
+            $this->getHttpClient($server->node)->post(
+                sprintf('/api/servers/%s/power', $server->uuid),
+                ['json' => ['action' => 'start']]
+            );
             $this->eventDispatcher->dispatch(new ServerInstalled($server));
         }
 
+
         return new JsonResponse([], Response::HTTP_NO_CONTENT);
+    }
+
+    public function getHttpClient(Node $node, array $headers = []): Client
+    {
+        Assert::isInstanceOf($node, Node::class);
+
+        return new Client([
+            'verify' => $this->app->environment('production'),
+            'base_uri' => $node->getConnectionAddress(),
+            'timeout' => config('pterodactyl.guzzle.timeout'),
+            'connect_timeout' => config('pterodactyl.guzzle.connect_timeout'),
+            'headers' => array_merge($headers, [
+                'Authorization' => 'Bearer ' . $node->getDecryptedKey(),
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+            ]),
+        ]);
     }
 }
